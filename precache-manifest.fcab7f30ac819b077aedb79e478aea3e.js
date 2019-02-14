f29110ee07fa06cc23f1cf0b3899ff6dbbbded60
Tunji Ologun <estolog@gmail.com>
2019-02-14 10:22:19 +0100

    Updates

---
+++ precache-manifest.fcab7f30ac819b077aedb79e478aea3e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "76902e4cd5e16c94125f",
+    "url": "/CounterApp/static/js/runtime~main.76902e4c.js"
+  },
+  {
+    "revision": "6bd2dd7654bccf1593de",
+    "url": "/CounterApp/static/js/main.6bd2dd76.chunk.js"
+  },
+  {
+    "revision": "512ee873752df540481a",
+    "url": "/CounterApp/static/js/1.512ee873.chunk.js"
+  },
+  {
+    "revision": "6bd2dd7654bccf1593de",
+    "url": "/CounterApp/static/css/main.d9a5b18b.chunk.css"
+  },
+  {
+    "revision": "512ee873752df540481a",
+    "url": "/CounterApp/static/css/1.07971b37.chunk.css"
+  },
+  {
+    "revision": "364c48a9b1a94bea6b31d30f1013de1a",
+    "url": "/CounterApp/index.html"
+  }
+];
